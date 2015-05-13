79eda2b4d206b510146d0a3a03b295ad44ae232d
Josh Harries <joshharries@outlook.com>
2015-05-13 20:54:11 +0100

    Form Changes + New Splash Screen

--- Bettys Form - v1/Bettys/My Project/Resources.Designer.vb
+++ Bettys Form - v1/Bettys/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property cupcake_clip_art_cupcake6() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("cupcake-clip-art-cupcake6", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property jhlogo() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("jhlogo", resourceCulture)
