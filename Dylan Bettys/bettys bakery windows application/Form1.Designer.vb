29265cc02d111a90883d976e112f1d056ba444ea
DylanT <JOSHHARRIES@OUTOOK.COM>
2015-06-10 11:56:28 +0100

    Added Dylans Files

---
+++ Dylan Bettys/bettys bakery windows application/Form1.Designer.vb
@@ -0,0 +1,446 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.smallFlourPack = New System.Windows.Forms.Label()
+        Me.largeFlourPack = New System.Windows.Forms.Label()
+        Me.mediumFlourPack = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.smallSugarPack = New System.Windows.Forms.Label()
+        Me.largeSugarPack = New System.Windows.Forms.Label()
+        Me.mediumSugarPack = New System.Windows.Forms.Label()
+        Me.largeButterPack = New System.Windows.Forms.Label()
+        Me.mediumButterPack = New System.Windows.Forms.Label()
+        Me.smallButterPack = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.drizzleCakeAmmount = New System.Windows.Forms.NumericUpDown()
+        Me.cupCakeAmmount = New System.Windows.Forms.NumericUpDown()
+        Me.label = New System.Windows.Forms.Label()
+        Me.totalFlour = New System.Windows.Forms.Label()
+        Me.totalButter = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.totalSugar = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.smallEggBox = New System.Windows.Forms.Label()
+        Me.mediumEggBox = New System.Windows.Forms.Label()
+        Me.largeEggBox = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.totalEggs = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.drizzleCakeAmmount, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.cupCakeAmmount, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(204, 370)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'smallFlourPack
+        '
+        Me.smallFlourPack.AutoSize = True
+        Me.smallFlourPack.Location = New System.Drawing.Point(331, 208)
+        Me.smallFlourPack.Name = "smallFlourPack"
+        Me.smallFlourPack.Size = New System.Drawing.Size(13, 13)
+        Me.smallFlourPack.TabIndex = 1
+        Me.smallFlourPack.Text = "0"
+        '
+        'largeFlourPack
+        '
+        Me.largeFlourPack.AutoSize = True
+        Me.largeFlourPack.Location = New System.Drawing.Point(144, 205)
+        Me.largeFlourPack.Name = "largeFlourPack"
+        Me.largeFlourPack.Size = New System.Drawing.Size(13, 13)
+        Me.largeFlourPack.TabIndex = 2
+        Me.largeFlourPack.Text = "0"
+        '
+        'mediumFlourPack
+        '
+        Me.mediumFlourPack.AutoSize = True
+        Me.mediumFlourPack.Location = New System.Drawing.Point(232, 208)
+        Me.mediumFlourPack.Name = "mediumFlourPack"
+        Me.mediumFlourPack.Size = New System.Drawing.Size(13, 13)
+        Me.mediumFlourPack.TabIndex = 3
+        Me.mediumFlourPack.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter amount of drizzle cakes"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(185, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Enter amount of chocolate cup cakes"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(51, 208)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Flour pack(s)"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(221, 173)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Medium"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(123, 173)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 13)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Large"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(318, 173)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(32, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Small"
+        '
+        'smallSugarPack
+        '
+        Me.smallSugarPack.AutoSize = True
+        Me.smallSugarPack.Location = New System.Drawing.Point(331, 248)
+        Me.smallSugarPack.Name = "smallSugarPack"
+        Me.smallSugarPack.Size = New System.Drawing.Size(13, 13)
+        Me.smallSugarPack.TabIndex = 12
+        Me.smallSugarPack.Text = "0"
+        '
+        'largeSugarPack
+        '
+        Me.largeSugarPack.AutoSize = True
+        Me.largeSugarPack.Location = New System.Drawing.Point(144, 248)
+        Me.largeSugarPack.Name = "largeSugarPack"
+        Me.largeSugarPack.Size = New System.Drawing.Size(13, 13)
+        Me.largeSugarPack.TabIndex = 13
+        Me.largeSugarPack.Text = "0"
+        '
+        'mediumSugarPack
+        '
+        Me.mediumSugarPack.AutoSize = True
+        Me.mediumSugarPack.Location = New System.Drawing.Point(232, 251)
+        Me.mediumSugarPack.Name = "mediumSugarPack"
+        Me.mediumSugarPack.Size = New System.Drawing.Size(13, 13)
+        Me.mediumSugarPack.TabIndex = 14
+        Me.mediumSugarPack.Text = "0"
+        '
+        'largeButterPack
+        '
+        Me.largeButterPack.AutoSize = True
+        Me.largeButterPack.Location = New System.Drawing.Point(144, 282)
+        Me.largeButterPack.Name = "largeButterPack"
+        Me.largeButterPack.Size = New System.Drawing.Size(13, 13)
+        Me.largeButterPack.TabIndex = 15
+        Me.largeButterPack.Text = "0"
+        '
+        'mediumButterPack
+        '
+        Me.mediumButterPack.AutoSize = True
+        Me.mediumButterPack.Location = New System.Drawing.Point(232, 285)
+        Me.mediumButterPack.Name = "mediumButterPack"
+        Me.mediumButterPack.Size = New System.Drawing.Size(13, 13)
+        Me.mediumButterPack.TabIndex = 16
+        Me.mediumButterPack.Text = "0"
+        '
+        'smallButterPack
+        '
+        Me.smallButterPack.AutoSize = True
+        Me.smallButterPack.Location = New System.Drawing.Point(331, 282)
+        Me.smallButterPack.Name = "smallButterPack"
+        Me.smallButterPack.Size = New System.Drawing.Size(13, 13)
+        Me.smallButterPack.TabIndex = 17
+        Me.smallButterPack.Text = "0"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(51, 282)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(73, 13)
+        Me.Label15.TabIndex = 18
+        Me.Label15.Text = "Butter pack(s)"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(56, 282)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(0, 13)
+        Me.Label16.TabIndex = 19
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(51, 248)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(73, 13)
+        Me.Label17.TabIndex = 20
+        Me.Label17.Text = "Sugar pack(s)"
+        '
+        'drizzleCakeAmmount
+        '
+        Me.drizzleCakeAmmount.Location = New System.Drawing.Point(224, 26)
+        Me.drizzleCakeAmmount.Name = "drizzleCakeAmmount"
+        Me.drizzleCakeAmmount.Size = New System.Drawing.Size(120, 20)
+        Me.drizzleCakeAmmount.TabIndex = 21
+        '
+        'cupCakeAmmount
+        '
+        Me.cupCakeAmmount.Location = New System.Drawing.Point(224, 66)
+        Me.cupCakeAmmount.Name = "cupCakeAmmount"
+        Me.cupCakeAmmount.Size = New System.Drawing.Size(120, 20)
+        Me.cupCakeAmmount.TabIndex = 22
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Location = New System.Drawing.Point(53, 134)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(54, 13)
+        Me.label.TabIndex = 23
+        Me.label.Text = "Total flour"
+        '
+        'totalFlour
+        '
+        Me.totalFlour.AutoSize = True
+        Me.totalFlour.Location = New System.Drawing.Point(116, 134)
+        Me.totalFlour.Name = "totalFlour"
+        Me.totalFlour.Size = New System.Drawing.Size(13, 13)
+        Me.totalFlour.TabIndex = 24
+        Me.totalFlour.Text = "0"
+        '
+        'totalButter
+        '
+        Me.totalButter.AutoSize = True
+        Me.totalButter.Location = New System.Drawing.Point(331, 134)
+        Me.totalButter.Name = "totalButter"
+        Me.totalButter.Size = New System.Drawing.Size(13, 13)
+        Me.totalButter.TabIndex = 25
+        Me.totalButter.Text = "0"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(262, 134)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(61, 13)
+        Me.Label19.TabIndex = 26
+        Me.Label19.Text = "Total butter"
+        '
+        'totalSugar
+        '
+        Me.totalSugar.AutoSize = True
+        Me.totalSugar.Location = New System.Drawing.Point(232, 134)
+        Me.totalSugar.Name = "totalSugar"
+        Me.totalSugar.Size = New System.Drawing.Size(13, 13)
+        Me.totalSugar.TabIndex = 27
+        Me.totalSugar.Text = "0"
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(157, 134)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(60, 13)
+        Me.Label21.TabIndex = 28
+        Me.Label21.Text = "Total sugar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(53, 322)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 29
+        Me.Label3.Text = "Egg(s)"
+        '
+        'smallEggBox
+        '
+        Me.smallEggBox.AutoSize = True
+        Me.smallEggBox.Location = New System.Drawing.Point(331, 322)
+        Me.smallEggBox.Name = "smallEggBox"
+        Me.smallEggBox.Size = New System.Drawing.Size(13, 13)
+        Me.smallEggBox.TabIndex = 30
+        Me.smallEggBox.Text = "0"
+        '
+        'mediumEggBox
+        '
+        Me.mediumEggBox.AutoSize = True
+        Me.mediumEggBox.Location = New System.Drawing.Point(232, 322)
+        Me.mediumEggBox.Name = "mediumEggBox"
+        Me.mediumEggBox.Size = New System.Drawing.Size(13, 13)
+        Me.mediumEggBox.TabIndex = 31
+        Me.mediumEggBox.Text = "0"
+        '
+        'largeEggBox
+        '
+        Me.largeEggBox.AutoSize = True
+        Me.largeEggBox.Location = New System.Drawing.Point(144, 322)
+        Me.largeEggBox.Name = "largeEggBox"
+        Me.largeEggBox.Size = New System.Drawing.Size(13, 13)
+        Me.largeEggBox.TabIndex = 32
+        Me.largeEggBox.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(364, 134)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(57, 13)
+        Me.Label11.TabIndex = 33
+        Me.Label11.Text = "Total eggs"
+        '
+        'totalEggs
+        '
+        Me.totalEggs.AutoSize = True
+        Me.totalEggs.Location = New System.Drawing.Point(427, 134)
+        Me.totalEggs.Name = "totalEggs"
+        Me.totalEggs.Size = New System.Drawing.Size(13, 13)
+        Me.totalEggs.TabIndex = 34
+        Me.totalEggs.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(335, 369)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(74, 23)
+        Me.Button2.TabIndex = 35
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(490, 416)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.totalEggs)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.largeEggBox)
+        Me.Controls.Add(Me.mediumEggBox)
+        Me.Controls.Add(Me.smallEggBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.totalSugar)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.totalButter)
+        Me.Controls.Add(Me.totalFlour)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.cupCakeAmmount)
+        Me.Controls.Add(Me.drizzleCakeAmmount)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.smallButterPack)
+        Me.Controls.Add(Me.mediumButterPack)
+        Me.Controls.Add(Me.largeButterPack)
+        Me.Controls.Add(Me.mediumSugarPack)
+        Me.Controls.Add(Me.largeSugarPack)
+        Me.Controls.Add(Me.smallSugarPack)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.mediumFlourPack)
+        Me.Controls.Add(Me.largeFlourPack)
+        Me.Controls.Add(Me.smallFlourPack)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.drizzleCakeAmmount, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.cupCakeAmmount, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents smallFlourPack As System.Windows.Forms.Label
+    Friend WithEvents largeFlourPack As System.Windows.Forms.Label
+    Friend WithEvents mediumFlourPack As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents smallSugarPack As System.Windows.Forms.Label
+    Friend WithEvents largeSugarPack As System.Windows.Forms.Label
+    Friend WithEvents mediumSugarPack As System.Windows.Forms.Label
+    Friend WithEvents largeButterPack As System.Windows.Forms.Label
+    Friend WithEvents mediumButterPack As System.Windows.Forms.Label
+    Friend WithEvents smallButterPack As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents drizzleCakeAmmount As System.Windows.Forms.NumericUpDown
+    Friend WithEvents cupCakeAmmount As System.Windows.Forms.NumericUpDown
+    Friend WithEvents label As System.Windows.Forms.Label
+    Friend WithEvents totalFlour As System.Windows.Forms.Label
+    Friend WithEvents totalButter As System.Windows.Forms.Label
+    Friend WithEvents Label19 As System.Windows.Forms.Label
+    Friend WithEvents totalSugar As System.Windows.Forms.Label
+    Friend WithEvents Label21 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents smallEggBox As System.Windows.Forms.Label
+    Friend WithEvents mediumEggBox As System.Windows.Forms.Label
+    Friend WithEvents largeEggBox As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents totalEggs As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Dylan Bettys/bettys bakery windows application/Form1.vb
@@ -0,0 +1,230 @@
+﻿Public Class Form1
+
+    Dim numberOfFlour As Integer
+    Dim smallFlourPacks As Integer
+    Dim largeFlourPacks As Integer
+    Dim mediumFlourPacks As Integer
+    Dim smallSugarPacks As Integer
+    Dim mediumSugarPacks As Integer
+    Dim largeSugarPacks As Integer
+    Dim smallButterPacks As Integer
+    Dim mediumButterPacks As Integer
+    Dim largeButterPacks As Integer
+    Dim numberOfButter As Integer
+    Dim numberOfSugar As Integer
+    Dim drizzleCakeFlour, cupCakeFlour, drizzleCakeSugar, cupCakeButter, drizzleCakeButter, cupCakeSugar, getCakes As Integer
+    Dim completeFlour As Integer
+    Dim drizzleCakeEgg, cupCakeEgg, totalEgg As Decimal
+    Dim smallEggBoxes, mediumEggBoxes, largeEggBoxes, completeSugar, completeButter As Integer
+
+
+    Sub ammountOfFlour()
+
+        drizzleCakeFlour = drizzleCakeAmmount.Value * 240
+        cupCakeFlour = cupCakeAmmount.Value * 12
+        completeFlour = cupCakeFlour + drizzleCakeFlour
+    End Sub
+
+    Sub ammountOfSugar()
+
+        drizzleCakeSugar = drizzleCakeAmmount.Value * 300
+        cupCakeSugar = cupCakeAmmount.Value * 14
+        completeSugar = cupCakeSugar + drizzleCakeSugar
+    End Sub
+
+    Sub ammountOfButter()
+
+        drizzleCakeButter = drizzleCakeAmmount.Value * 240
+        cupCakeButter = cupCakeAmmount.Value * 4
+        completeButter = cupCakeButter + drizzleCakeButter
+    End Sub
+
+    Sub ammountOfEggs()
+
+        drizzleCakeEgg = drizzleCakeAmmount.Value * 4.5
+        cupCakeEgg = cupCakeAmmount.Value * 0.1
+        totalEgg = cupCakeEgg + drizzleCakeEgg
+
+    End Sub
+
+    Sub displayCustomerTotals()
+
+        totalFlour.Text = completeFlour
+        totalSugar.Text = completeSugar
+        totalButter.Text = completeButter
+        totalEggs.Text = totalEgg
+    End Sub
+
+
+    Sub calculateFlour()
+
+        smallFlourPacks = Math.Floor(completeFlour / 250)
+
+        If completeFlour Mod 250 <> 0 Then
+            smallFlourPacks += 1
+        End If
+
+        While smallFlourPacks > 2
+            smallFlourPacks -= 3
+            largeFlourPacks += 1
+        End While
+
+        While smallFlourPacks > 1
+            smallFlourPacks -= 2
+            mediumFlourPacks += 1
+        End While
+
+    End Sub
+
+    Sub calculateSugar()
+
+        smallSugarPacks = Math.Floor(completeSugar / 200)
+
+        If numberOfSugar Mod 200 <> 0 Then
+            smallSugarPacks += 1
+        End If
+
+        While smallSugarPacks > 2
+            smallSugarPacks -= 3
+            largeSugarPacks += 1
+        End While
+
+        While smallSugarPacks > 1
+            smallSugarPacks -= 2
+            mediumSugarPacks += 1
+        End While
+
+    End Sub
+
+    Sub calculateButter()
+
+        numberOfButter = Console.ReadLine()
+        smallButterPacks = Math.Floor(completeButter / 125)
+
+        If numberOfButter Mod 125 <> 0 Then
+            smallButterPacks += 1
+        End If
+
+        While smallButterPacks > 3
+            smallButterPacks -= 4
+            largeButterPacks += 1
+        End While
+
+        While smallButterPacks > 1
+            smallButterPacks -= 2
+            mediumButterPacks += 1
+        End While
+
+    End Sub
+
+    Sub calculateEgg()
+
+        largeEggBoxes = 0
+        mediumEggBoxes = 0
+        smallEggBoxes = 0
+        Dim six, ten As Integer
+        While totalEgg > 5
+
+            six = 6
+            ten = 10
+
+            While totalEgg > six
+                six += 6
+            End While
+
+            While totalEgg > ten
+                ten += 10
+            End While
+
+            six -= totalEgg
+            ten -= totalEgg
+            If six <= ten Then
+                smallEggBoxes += 1
+                totalEgg -= 6
+
+            Else
+                mediumEggBoxes += 1
+                totalEgg -= 10
+            End If
+        End While
+
+
+
+        If totalEgg > 0 And totalEgg < 7 Then
+            smallEggBoxes += 1
+
+        ElseIf totalEgg > 6 Then
+            mediumEggBoxes += 1
+        End If
+
+        While smallEggBoxes > 1
+            largeEggBoxes += 1
+            smallEggBoxes -= 2
+
+        End While
+
+    End Sub
+
+    Sub roundEggs()
+
+        Math.Ceiling(totalEgg)
+
+    End Sub
+
+    Sub displayTotal()
+
+        smallFlourPack.Text = smallFlourPacks
+        mediumFlourPack.Text = mediumFlourPacks
+        largeFlourPack.Text = largeFlourPacks
+        smallSugarPack.Text = smallSugarPacks
+        mediumSugarPack.Text = mediumSugarPacks
+        largeSugarPack.Text = largeSugarPacks
+        smallButterPack.Text = smallButterPacks
+        mediumButterPack.Text = mediumButterPacks
+        largeButterPack.Text = largeButterPacks
+        smallEggBox.Text = smallEggBoxes
+        mediumEggBox.Text = mediumEggBoxes
+        largeEggBox.Text = largeEggBoxes
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        ammountOfButter()
+        ammountOfFlour()
+        ammountOfSugar()
+        ammountOfEggs()
+        displayTotal()
+        displayCustomerTotals()
+        roundEggs()
+        calculateButter()
+        calculateFlour()
+        calculateSugar()
+        calculateEgg()
+        displayTotal()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        drizzleCakeAmmount.Value = 0
+        cupCakeAmmount.Value = 0
+        Button1_Click(sender, New System.EventArgs())
+    End Sub
+
+End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+  
